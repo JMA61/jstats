f10b03dd0163e00e21b0d41313d6d10825467e5a
Jeff Ackerman <jmaMedia@outlook.com>
2026-06-11 06:58:16 +1000

    New package - jstats - mentioned in zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -265,7 +265,8 @@
 
   # If the gist says a successor exists, show migration message only.
   # Otherwise, run the standard GitHub version check.
-  if (!is.null(gist_info) && !is.null(gist_info$successor)) {
+  if (!is.null(gist_info) && !is.null(gist_info$successor) &&
+      !identical(gist_info$successor$package, pkgname)) {
     .jst_show_migration(gist_info$successor, installed_ver)
   } else {
     .jst_show_version_status(installed_ver)
